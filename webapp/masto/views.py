f6692f0d6580bdd66c1647f3091df192a85c3f3f
Andreas <andreas@neumeier.org>
2023-11-06 16:47:17 +0000

    urls.

--- webapp/masto/views.py
+++ webapp/masto/views.py
@@ -17,24 +17,7 @@ class MastoOAuth2Adapter(OAuth2Adapter):
 
     access_token_url = "https://mastodon.social/oauth/token"
     profile_url = "https://mastodon.social/oauth/token"
-
-    @property
-    def authorize_url(self):
-        settings = self.get_provider().get_settings()
-        url = settings.get(
-            "AUTHORIZE_URL", "https://mastodon.social/oauth/authorize"
-        )
-        return url
-
-    def complete_login(self, request, app, token, **kwargs):
-        headers = {"Content-Type": "application/json"}
-        response = requests.get(
-            "{0}/{1}".format(self.profile_url, token.token), headers=headers
-        )
-        response.raise_for_status()
-        extra_data = response.json()
-        return self.get_provider().sociallogin_from_response(request, extra_data)
-
+    authorize_url = "https://mastodon.social/oauth/authorize"
 
 oauth2_login = OAuth2LoginView.adapter_view(MastoOAuth2Adapter)
 oauth2_callback = OAuth2CallbackView.adapter_view(MastoOAuth2Adapter)
