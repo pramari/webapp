47f1974e7558758d88989c1bdf2aca5a40717602
Andreas <andreas@neumeier.org>
2023-11-30 20:27:13 +0000

    removed HS

--- tests/testsettings.py
+++ tests/testsettings.py
@@ -1,4 +1,20 @@
 SECRET_KEY = "fake-key"
 INSTALLED_APPS = [
     "webapp",
+    "django.contrib.contenttypes",
+    "django.contrib.sites",
+    "django.contrib.auth",
+    "allauth",
+    "allauth.account",
+    "allauth.socialaccount",
 ]
+MIDDLEWARE = [
+    "allauth.account.middleware.AccountMiddleware",
+]
+AUTH_USER_MODEL = "webapp.User"
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
+    }
+}
