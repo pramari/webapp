1e5c4630fae790336cf24f67947338e0b2855e9a
Andreas <andreas@neumeier.org>
2023-12-14 11:57:13 +0000

    bring back activitypub, refactor out.

--- webapp/urls.py
+++ webapp/urls.py
@@ -32,15 +32,6 @@ from webapp.views import (
     SearchView,
 )
 
-# from webapp.ActivityView import (
-#    WebFingerView,
-#    ActorView,
-#    InboxView,
-#    OutboxView,
-#    FollowView,
-#    FollowersView,
-#    FollowingView
-# )
 
 
 logger = logging.getLogger(__name__)
@@ -85,42 +76,6 @@ urlpatterns += [
 
 urlpatterns += [
     path(r"o/", include("oauth2_provider.urls", namespace="oauth2_provider"))
-]
-
-urlpatterns += [
-    # path('.well-known/webfinger', WebFingerView.as_view(), name='webfinger'),
-    # path('activity/', include('actstream.urls')),
-    # path('inbox/', InboxView.as_view(), name='profile-inbox'),
-    # path(
-    #     r'accounts/<slug:slug>/inbox',
-    #     InboxView.as_view(),
-    #     name='profile-inbox'
-    # ),
-    # path('outbox/', OutboxView.as_view(), name='profile-outbox'),
-    # path(
-    #     r'accounts/<slug:slug>/outbox',
-    #     OutboxView.as_view(),
-    #     name='profile-outbox'
-    # ),
-    # path('follow/', FollowView.as_view(), name='profile-follow'),
-    # path(
-    #    'activity/<str:activity_id>/',
-    #    ActivityView.as_view(),
-    #    name='activity-detail'
-    # ),
-    # path(
-    #    r'accounts/<slug:slug>/actor',
-    #    ActorView.as_view(),
-    #    name='activity-view'
-    # ),
-    # path(r'accounts/<slug:slug>/followers',
-    #     FollowersView.as_view(),
-    #     name='profile-followers'
-    #     ),
-    # path(r'accounts/<slug:slug>/following',
-    #     FollowingView.as_view(),
-    #     name='profile-following'
-    #     ),
 ]
 
 urlpatterns += [
