ca32f6b15953250a8209ed56e9e9df6393b4e60a
Andreas Neumeier <andreas@neumeier.org>
2024-09-28 07:19:36 +0000

    a bit of cleanup, remove activitypub from the project, add renderer for JSON-LD

---
+++ webapp/renderers.py
@@ -0,0 +1,17 @@
+from rest_framework import renderers
+
+class JsonLDRenderer(renderers.BaseRenderer):
+    """JsonLDRenderer
+
+    Args:
+        renderers (JsonLDRenderer): Renders JSON-LD data
+
+    Returns:
+        JsonLD: Added Context to JSON-LD data
+    """
+    media_type = 'application/ld+json; profile="https://www.w3.org/ns/activitystreams"'
+    charset = 'utf-8'
+
+    def render(self, data, accepted_media_type=None, renderer_context=None):
+        data['@context'] = "https://www.w3.org/ns/activitystreams"
+        return data.encode(self.charset)
